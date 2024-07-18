a8d43ee72a6a934ba71865d5dc1c47c36cc03aba
shubham kumar <theshubham.dev@gmail.com>
2024-07-19 01:48:38 +0530

    fix

--- src/api/RestApi.js
+++ src/api/RestApi.js
@@ -1,7 +1,6 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  // baseURL: 'https://sapthapadhi.bloomitsolutions.co.in',
-  baseURL: 'http://localhost:8800',
+  baseURL: 'https://sapthapadhi.bloomitsolutions.co.in',
 });
-// baseURL:"http://localhost:3300"
+// baseURL:"http://localhost:8800"
