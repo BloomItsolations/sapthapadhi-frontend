5fd303e0f7c558503b34522824c22bc539d95ee0
unknown <shivamanand8969@gmail.com>
2024-11-13 17:27:21 +0530

    update api rul

--- src/api/RestApi.js
+++ src/api/RestApi.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  baseURL:"http://143.244.143.177:8800"
+  baseURL:"http://97.74.93.184"
 });
 // baseURL:"http://localhost:8800"
 //  baseURL: 'https://sapthapadhi.bloomitsolutions.co.in',
