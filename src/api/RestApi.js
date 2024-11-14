68b1ee485ab61f9552e464dddcd2e0bb7f7cd51d
unknown <shivamanand8969@gmail.com>
2024-11-14 18:06:39 +0530

    udate api url

--- src/api/RestApi.js
+++ src/api/RestApi.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  baseURL:"https://api.sapthapadhimatrimony.in/"  
+  baseURL:"https://sapthapadhimatrimony.in/backend"  
 });
 // baseURL:"http://localhost:8800"
 //  baseURL: 'https://sapthapadhi.bloomitsolutions.co.in',
